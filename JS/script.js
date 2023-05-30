5425289866200cc84b45f81bc18b48f5cc6fb2ff
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-30 15:48:34 +0200

    added images to html

---
+++ JS/script.js
@@ -0,0 +1,54 @@
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+images.forEach((element) => {
+ 
+    const carousel = document.getElementById("carousel")
+
+    const newDiv = document.createElement("div")    //Ho creato 5 div all'intenro del div con classe carousel
+
+
+
+
+    const imageElement = document.createElement("img"); // ho aggiunto ai div appena creati l'immagine corrispondente in base all'oggetto dell'array images
+
+    imageElement.src = element.image;
+
+    newDiv.appendChild(imageElement);
+
+    const titleElement = document.createElement("h2"); // ho aggiunto ai div appena creati il titolo corrispondente in base all'oggetto dell'array images
+
+    titleElement.textContent = element.title;
+
+    newDiv.appendChild(titleElement);
+
+    const textElement = document.createElement("p"); // ho aggiunto ai div appena creati il testo corrispondente in base all'oggetto dell'array images
+
+    textElement.textContent = element.text;
+
+    newDiv.appendChild(textElement);
+
+    carousel.appendChild(newDiv)
+
+})
